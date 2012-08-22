fcc648f921cb2764c180f05d62a1812af5a277a3
John P. Bloch <john@avendimedia.com>
2012-08-22 10:30:30 -0400

    Add our walker class

--- MenuTamerClass.php
+++ MenuTamerClass.php
@@ -4,6 +4,7 @@ class MenuTamer {
 
 	public static function init() {
 		self::load_l10n();
+		add_filter( 'wp_edit_nav_menu_walker', array( __CLASS__, 'nav_edit_walker' ) );
 		wp_enqueue_script( 'menu-tamer', plugins_url( 'js/menu-tamer.js', __FILE__ ), array( 'nav-menu' ), '0.1', true );
 		wp_localize_script( 'menu-tamer', 'MenuTamerL10n', array(
 			'collapse' => __( 'Collapse', 'menu-tamer' ),
@@ -13,6 +14,11 @@ class MenuTamer {
 
 	protected static function load_l10n() {
 		load_plugin_textdomain( 'menu-tamer', false, dirname( MT_PLUGIN_BASENAME ) . '/lang' );
+	}
+
+	public static function nav_edit_walker() {
+		require dirname(__FILE__).'/MenuTamerWalkerClass.php';
+		return 'Walker_MenuTamer_Nav_Menu_Edit';
 	}
 
 }
