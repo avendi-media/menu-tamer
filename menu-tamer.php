ebbf783027bb3eb31d8e8c97f98a4af5574b8490
John P. Bloch <john@avendimedia.com>
2012-08-22 09:41:36 -0400

    Define a constant for the plugin basename of our plugin

--- menu-tamer.php
+++ menu-tamer.php
@@ -6,6 +6,7 @@
  * Description: Make your nav menus easier to work with
  */
 if ( is_admin() && (!defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
+	define( 'MT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 	require dirname( __FILE__ ) . '/MenuTamerClass.php';
 	add_action( 'init', array( 'MenuTamer', 'init' ) );
 }
