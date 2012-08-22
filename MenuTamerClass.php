277fe65da706d1196247031c00dfc225ace3e975
John P. Bloch <john@avendimedia.com>
2012-08-22 09:42:01 -0400

    Method to initialize l10n for the plugin

--- MenuTamerClass.php
+++ MenuTamerClass.php
@@ -3,7 +3,11 @@
 class MenuTamer {
 
 	public static function init() {
-		
+		self::load_l10n();
+	}
+
+	protected static function load_l10n() {
+		load_plugin_textdomain( 'menu-tamer', false, dirname( MT_PLUGIN_BASENAME ) . '/lang' );
 	}
 
 }
