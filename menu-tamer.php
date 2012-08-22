430448103ce073018aebd87536a72becc0bd69cc
John P. Bloch <john@avendimedia.com>
2012-08-22 13:33:46 -0400

    Add more plugin headers and license info

--- menu-tamer.php
+++ menu-tamer.php
@@ -1,10 +1,32 @@
 <?php
 
-/**
- * Plugin Name: Menu Tamer
- * Version: 0.1
- * Description: Make your nav menus easier to work with
- */
+/*
+Plugin Name: Menu Tamer
+Version: 0.1
+Description: Make your nav menus easier to work with
+Plugin URI: http://github.com/avendi-media/menu-tamer
+Author: Avendi Media
+Author URI: http://www.avendimedia.com/
+License: GPLv2 or later
+*/
+/*
+Copyright (C) 2012  Avendi Media, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 if ( is_admin() ) {
 	define( 'MT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 	require dirname( __FILE__ ) . '/MenuTamerClass.php';
