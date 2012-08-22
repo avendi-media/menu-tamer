ebdd8bb48dbcee8ecdec2914e326ce0fee4670ce
John P. Bloch <john@avendimedia.com>
2012-08-22 10:02:07 -0400

    Enqueue the javascript

--- MenuTamerClass.php
+++ MenuTamerClass.php
@@ -4,6 +4,7 @@ class MenuTamer {
 
 	public static function init() {
 		self::load_l10n();
+		wp_enqueue_script( 'menu-tamer', plugins_url( 'js/menu-tamer.js', __FILE__ ), array( 'nav-menu' ), '0.1', true );
 	}
 
 	protected static function load_l10n() {
