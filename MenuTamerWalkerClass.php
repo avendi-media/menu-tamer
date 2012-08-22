803abea29553c7d272e71aa30855791f18c82eeb
John P. Bloch <john@avendimedia.com>
2012-08-22 10:48:00 -0400

    Let's rely on the javascript to set the text. It'll make i18n easier to manage

--- MenuTamerWalkerClass.php
+++ MenuTamerWalkerClass.php
@@ -8,7 +8,7 @@ class Walker_MenuTamer_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
 		$location = strpos( $tempOutput, '<a class="item-edit"' );
 		$parts = array(
 			substr( $tempOutput, 0, $location ),
-			'<a href="#" class="item-type hide-if-no-js hidden menuTamerToggle">Collapse</a>',
+			'<a href="#" class="item-type hide-if-no-js hidden menuTamerToggle"></a>',
 			"\n\t\t\t\t\t\t",
 			substr( $tempOutput, $location ),
 		);
