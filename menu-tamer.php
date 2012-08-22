7f9e71a2b5e3f16e3f8b50b9d8c03abc44139ffc
John P. Bloch <john@avendimedia.com>
2012-08-22 09:34:34 -0400

    Get our class if we're in the admin

--- menu-tamer.php
+++ menu-tamer.php
@@ -1,7 +1,10 @@
 <?php
+
 /**
  * Plugin Name: Menu Tamer
  * Version: 0.1
  * Description: Make your nav menus easier to work with
  */
-
+if ( is_admin() && (!defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
+	require dirname( __FILE__ ) . '/MenuTamerClass.php';
+}
